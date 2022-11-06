9a64cb78095946c7bf861a575349f7a39572794d
shivamjjha <shivamjjha@gmail.com>
2022-11-06 21:07:00 +0530

    Home loader, successfully creating a new order and retriving past orders from stored cookie

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,98 +2,89 @@ import AddOrderModal from '../components/AddOrderModal';
 import { getCookie, setCookie } from '../utils/cookie';
 import { Dialog, Transition } from '@headlessui/react';
 import { Fragment, useRef, useState } from 'react';
-import {
-  useFetcher,
-  Form,
-  useLoaderData
-} from 'react-router-dom';
+import { useFetcher, Form, useLoaderData } from 'react-router-dom';
+
 export async function action({ request, params }: any) {
-  console.log('action')
+  console.log('action');
   const ordersFromCookie = getCookie('orders');
-  const existingOrders = ordersFromCookie
-    ? JSON.parse(ordersFromCookie)
-    : [];
-  console.log('existingOrders', existingOrders)
+  const existingOrders = ordersFromCookie ? JSON.parse(ordersFromCookie) : [];
+  console.log('existingOrders', existingOrders);
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  console.log('data', data)
+  console.log('data', data);
 
+  const record = data;
+  const newOrdersList = [...existingOrders, record];
+  console.log('newRestaurantsList', newOrdersList);
 
-  // const record = JSON.parse(data?.data);
-  // const newRestaurantsList = [...existingOrders, record];
+  setCookie('orders', JSON.stringify(newOrdersList));
+  return;
+}
 
-  // setCookie('orders', JSON.stringify(newRestaurantsList));
-  return
+export async function loader() {
+  const ordersFromCookie = getCookie('orders');
+  const existingOrders = ordersFromCookie ? JSON.parse(ordersFromCookie) : [];
+
+  return existingOrders;
 }
 
 export function Home() {
-  const data: any = useLoaderData();
+  const orders: any[] | undefined = useLoaderData() as any;
   const [selected, setSelected] = useState<any>(null);
-  const [open, setOpen] = useState(false);
-  const cancelButtonRef = useRef(null);
-  const fetcher = useFetcher()
+
+  console.log('data', orders);
 
   return (
-    <div className="overflow-x-auto relative">
+    <div className='overflow-x-auto relative'>
       <AddOrderModal />
-      <table className="w-full text-sm text-left text-gray-500">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+      <table className='w-full text-sm text-left text-gray-500'>
+        <thead className='text-xs text-gray-700 uppercase bg-gray-50'>
           <tr>
-            <th scope="col" className="py-3 px-6">
+            <th scope='col' className='py-3 px-6'>
               Order Number
             </th>
-            <th scope="col" className="py-3 px-6">
+            <th scope='col' className='py-3 px-6'>
               Order Due Date
             </th>
-            <th scope="col" className="py-3 px-6">
+            <th scope='col' className='py-3 px-6'>
               Customer Buyer Name
             </th>
-            <th scope="col" className="py-3 px-6">
+            <th scope='col' className='py-3 px-6'>
               Customer Address
             </th>
-            <th scope="col" className="py-3 px-6">
+            <th scope='col' className='py-3 px-6'>
               Customer Phone
             </th>
-            <th scope="col" className="py-3 px-6">
+            <th scope='col' className='py-3 px-6'>
               Order Total
             </th>
-            <th scope="col" className="py-3 px-6 text-center" colSpan={2}>
+            <th scope='col' className='py-3 px-6 text-center' colSpan={2}>
               Actions
             </th>
           </tr>
         </thead>
         <tbody>
-          <tr className="bg-white border-b">
-            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap">
-              Apple MacBook Pro 17"
-            </th>
-            <td className="py-4 px-6">
-              Order Due Date
-            </td>
-            <td className="py-4 px-6">
-              Customer Buyer Name
-            </td>
-            <td className="py-4 px-6">
-              Customer Address
-            </td>
-            <td className="py-4 px-6">
-              Customer Phone
-            </td>
-            <td className="py-4 px-6">
-              Order Total
-            </td>
-            <td className="py-4 px-6">
-              Edit
-            </td>
-            <td className="py-4 px-6">
-              Delete
-            </td>
-          </tr>
+          {orders?.map((item: any, idx) => {
+            return (
+              <tr className='bg-white border-b' key={idx}>
+                <th
+                  scope='row'
+                  className='py-4 px-6 font-medium text-gray-900 whitespace-nowrap'
+                >
+                  {item.orderNumber ?? ''}
+                </th>
+                <td className='py-4 px-6'>{item.orderDueDate ?? ''}</td>
+                <td className='py-4 px-6'>{item.buyerName ?? ''}</td>
+                <td className='py-4 px-6'>{item.address ?? ''}</td>
+                <td className='py-4 px-6'>{item.tel ?? ''}</td>
+                <td className='py-4 px-6'>{item.orderTotal ?? ''}</td>
+                <td className='py-4 px-6'>Edit</td>
+                <td className='py-4 px-6'>Delete</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
-
-
-
   );
 }
