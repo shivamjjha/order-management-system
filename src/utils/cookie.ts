0bd519c76ddd184fff045444076040118eed7488
shivamjjha <shivamjjha@gmail.com>
2022-11-06 18:45:58 +0530

    utils to manipulate values stored in cookies

---
+++ src/utils/cookie.ts
@@ -0,0 +1,16 @@
+export const getCookie = (key: string) =>
+  ('; ' + document.cookie).split(`; ${key}=`).pop()?.split(';')[0] ?? null;
+
+export function setCookie(name: string, value: string, days = 30) {
+  let expires = '';
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = '; expires=' + date.toUTCString();
+  }
+  document.cookie = name + '=' + (value || '') + expires + '; path=/';
+}
+
+export function eraseCookie(name: string) {
+  document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
