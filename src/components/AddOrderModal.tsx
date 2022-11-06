bd49df11cfdfde8a0dee0cde10c295b90472ffa0
shivamjjha <shivamjjha@gmail.com>
2022-11-06 22:10:23 +0530

    fixed bug of just closing modal and not actually submitting form in edit modal

--- src/components/AddOrderModal.tsx
+++ src/components/AddOrderModal.tsx
@@ -21,13 +21,14 @@ export default function AddOrderModal() {
   const pathname = location.pathname;
   const orders: any[] | undefined = useLoaderData() as any;
 
-  console.log('AddOrderModal', orders);
+  // console.log('AddOrderModal', orders);
 
-  const open = pathname === '/new' || pathname.startsWith('/edit/');
+  const isEditPage = pathname.startsWith('/edit/')
+  const open = pathname === '/new' || isEditPage;
   const onClose = () => navigate('/');
 
   const currentOrder = orders?.find(item => item.orderNumber === orderNumber);
-  console.log('currentOrder', currentOrder);
+  // console.log('currentOrder', currentOrder);
   
 
   return (
@@ -90,9 +91,6 @@ export default function AddOrderModal() {
                                     name='orderNumber'
                                     defaultValue={currentOrder?.orderNumber ?? ''}
                                   />
-                                  {/* <p className='text-red-500 text-xs italic'>
-                                  Please fill out this field.
-                                </p> */}
                                 </div>
                                 <div className='w-full md:w-1/2 px-3'>
                                   <label
@@ -182,11 +180,11 @@ export default function AddOrderModal() {
                         </div>
                       </div>
                     </div>
+                    <input type="hidden" name="edit" value={isEditPage.toString()} />
                     <div className='bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6'>
                       <button
                         type='submit'
                         className='inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm'
-                        onClick={onClose}
                       >
                         Save
                       </button>
