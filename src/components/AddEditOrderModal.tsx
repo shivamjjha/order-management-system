7b76ff7d46230d71cad5646f8cde6d302475ae45
shivamjjha <shivamjjha@gmail.com>
2022-11-06 22:51:25 +0530

    UI

--- src/components/AddEditOrderModal.tsx
+++ src/components/AddEditOrderModal.tsx
@@ -180,7 +180,7 @@ export default function AddEditOrderModal() {
                     <div className='bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6'>
                       <button
                         type='submit'
-                        className='inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm'
+                        className='inline-flex w-full justify-center rounded-md border border-transparent bg-green-400 px-3 py-1 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm'
                       >
                         Save
                       </button>
@@ -200,8 +200,9 @@ export default function AddEditOrderModal() {
           </div>
         </Dialog>
       </Transition.Root>
-      {/* <button onClick={() => setOpen((o: any) => !o)}>New Order</button> */}
-      <Link to='/new'>New Order</Link>
+      <Link to='/new'
+        className='inline-flex w-full justify-center rounded-md border border-transparent bg-slate-400 px-3 py-1 text-base font-medium text-white shadow-sm hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm'
+      >New Order</Link>
     </>
   );
 }
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -9,9 +9,11 @@ export default function Root() {
 
   return (
     <>
-      <header>
-        <h1>Orders Page</h1>
-        <button type='button' onClick={logout}>Logout</button>
+      <header className='flex justify-between mb-2 mx-3'>
+        <h1 className='text-2xl'>Orders Page</h1>
+        <button type='button'
+          className='inline-flex w-full justify-center rounded-md border border-transparent bg-slate-400 px-3 py-1 text-base font-medium text-white shadow-sm hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm'
+          onClick={logout}>Logout</button>
       </header>
       <div
         id='detail'
