38ad3d38954860d015f025cbea433ee270ab49a1
shivamjjha <shivamjjha@gmail.com>
2022-11-06 20:14:07 +0530

    Add Order Modal Component

--- src/components/AddOrderModal.tsx
+++ src/components/AddOrderModal.tsx
@@ -41,10 +41,6 @@ export default function AddOrderModal() {
                 <Dialog.Panel className='relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
                   <div className='bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4'>
                     <div className='sm:flex sm:items-start'>
-                      <div className='mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10'>
-                        {/* <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" /> */}
-                        Icon
-                      </div>
                       <div className='mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left'>
                         <Dialog.Title
                           as='h3'
@@ -60,30 +56,29 @@ export default function AddOrderModal() {
                                   className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
                                   htmlFor='grid-first-name'
                                 >
-                                  First Name
+                                  Order Number
                                 </label>
                                 <input
-                                  className='appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+                                  className='appearance-none block w-full  text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
                                   id='grid-first-name'
                                   type='text'
-                                  placeholder='Jane'
                                 />
-                                <p className='text-red-500 text-xs italic'>
+                                {/* <p className='text-red-500 text-xs italic'>
                                   Please fill out this field.
-                                </p>
+                                </p> */}
                               </div>
                               <div className='w-full md:w-1/2 px-3'>
                                 <label
                                   className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
                                   htmlFor='grid-last-name'
                                 >
-                                  Last Name
+                                  Order Due Date
                                 </label>
                                 <input
-                                  className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+                                  className='appearance-none block w-full  text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
                                   id='grid-last-name'
-                                  type='text'
-                                  placeholder='Doe'
+                                  type='date'
+                                  placeholder='Select Order Date'
                                 />
                               </div>
                             </div>
@@ -91,75 +86,57 @@ export default function AddOrderModal() {
                               <div className='w-full px-3'>
                                 <label
                                   className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                                  htmlFor='grid-password'
+                                  htmlFor='c-buyer-name'
                                 >
-                                  Password
+                                  Customer Buyer Name
                                 </label>
                                 <input
-                                  className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                                  id='grid-password'
-                                  type='password'
-                                  placeholder='******************'
+                                  className='appearance-none block w-full  text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+                                  id='c-buyer-name'
+                                  type='text'
                                 />
-                                <p className='text-gray-600 text-xs italic'>
-                                  Make it as long and as crazy as you'd like
-                                </p>
+                              </div>
+                            </div>
+                            <div className='flex flex-wrap -mx-3 mb-6'>
+                              <div className='w-full px-3'>
+                                <label
+                                  className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
+                                  htmlFor='c-address'
+                                >
+                                  Customer Address
+                                </label>
+                                <input
+                                  className='appearance-none block w-full  text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+                                  id='c-address'
+                                  type='text'
+                                />
                               </div>
                             </div>
                             <div className='flex flex-wrap -mx-3 mb-2'>
-                              <div className='w-full md:w-1/3 px-3 mb-6 md:mb-0'>
+                              <div className='w-full md:w-2/3 px-3 mb-6 md:mb-0'>
                                 <label
                                   className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                                  htmlFor='grid-city'
+                                  htmlFor='c-phone'
                                 >
-                                  City
+                                  Customer Phone
                                 </label>
                                 <input
-                                  className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                                  id='grid-city'
-                                  type='text'
-                                  placeholder='Albuquerque'
+                                  className='appearance-none block w-full  text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+                                  id='c-phone'
+                                  type='tel'
                                 />
                               </div>
                               <div className='w-full md:w-1/3 px-3 mb-6 md:mb-0'>
                                 <label
                                   className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                                  htmlFor='grid-state'
+                                  htmlFor='order-total'
                                 >
-                                  State
-                                </label>
-                                <div className='relative'>
-                                  <select
-                                    className='block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                                    id='grid-state'
-                                  >
-                                    <option>New Mexico</option>
-                                    <option>Missouri</option>
-                                    <option>Texas</option>
-                                  </select>
-                                  <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700'>
-                                    <svg
-                                      className='fill-current h-4 w-4'
-                                      xmlns='http://www.w3.org/2000/svg'
-                                      viewBox='0 0 20 20'
-                                    >
-                                      <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
-                                    </svg>
-                                  </div>
-                                </div>
-                              </div>
-                              <div className='w-full md:w-1/3 px-3 mb-6 md:mb-0'>
-                                <label
-                                  className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                                  htmlFor='grid-zip'
-                                >
-                                  Zip
+                                  Order Total
                                 </label>
                                 <input
-                                  className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                                  id='grid-zip'
+                                  className='appearance-none block w-full  text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+                                  id='order-total'
                                   type='text'
-                                  placeholder={90210}
                                 />
                               </div>
                             </div>
@@ -174,7 +151,7 @@ export default function AddOrderModal() {
                       className='inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm'
                       onClick={() => setOpen(false)}
                     >
-                      Deactivate
+                      Save
                     </button>
                     <button
                       type='button'
